e2af46a8933fdbb1d56a6c6736295c9b60fdd6d6
EvgenyStralenya <https://github.com/EvgenyStralenya>
2020-06-30 00:01:46 +0300

    Updates

---
+++ precache-manifest.f7f13c001a8e866bb3a77c3c9984823a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fd4377a69cc11096dea7a06c558086",
+    "url": "/React-course-work/index.html"
+  },
+  {
+    "revision": "3bb901e7637b00d6685f",
+    "url": "/React-course-work/static/css/main.cf781ad0.chunk.css"
+  },
+  {
+    "revision": "e7dd2922ec331fd49e58",
+    "url": "/React-course-work/static/js/2.0b559108.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-course-work/static/js/2.0b559108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb901e7637b00d6685f",
+    "url": "/React-course-work/static/js/main.74aa3f34.chunk.js"
+  },
+  {
+    "revision": "13982ab0c8fc757635fe",
+    "url": "/React-course-work/static/js/runtime-main.25bc751e.js"
+  }
+]);
